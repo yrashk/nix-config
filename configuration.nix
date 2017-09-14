407f857bacd2c762013fdc35bcfeca52aa451603
Yurii Rashkovskii <yrashk@gmail.com>
2017-09-13 23:46:23 -0700

    Problem: sddm's login page doesn't look nice
    Solution: lightdm looks better

--- configuration.nix
+++ configuration.nix
@@ -83,11 +83,10 @@
   services.xserver.xkbOptions = "grp:caps_toggle,grp_led:caps";
 
   # Enable the KDE Desktop Environment.
-  services.xserver.displayManager.sddm = {
+  services.xserver.displayManager.lightdm = {
      enable = true;
   };
   services.xserver.windowManager.awesome.enable = true;
-  #services.xserver.desktopManager.plasma5.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.extraUsers.yrashk = {

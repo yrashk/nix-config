45301a0be2d262f7f0bb8afee447701f44b06e9c
Yurii Rashkovskii <yrashk@gmail.com>
2017-09-13 19:35:37 -0700

    Problem: using USB devices from within VirtualBox doesn't work
    Solution: add my user to the `vboxusers` group

--- configuration.nix
+++ configuration.nix
@@ -93,7 +93,7 @@
      isNormalUser = true;
      description = "Yurii Rashkovskii";
      home = "/home/yrashk";
-     extraGroups = ["wheel"];
+     extraGroups = ["wheel" "vboxusers"];
      uid = 1000;
      hashedPassword = "$6$YZ2znbV6G4$dbKGu7E/ywwHVmZy9Ez3nenesHncLyKqQKYwdWo9QTLerhDH3NCxleuT8fl5vDCGEzDQrLwpd/VMmDgy90D3q1"; 
      shell = "/run/current-system/sw/bin/fish";

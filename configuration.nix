2c4b0135e7f356d2f287cb3359e5dc04a2d14569
Yurii Rashkovskii <yrashk@gmail.com>
2017-09-14 00:00:35 -0700

    Problem: rebuilding with nix is slow
    Solution: at the moment, I have a lot of cores.
    
    Why not use more of them? nix.maxJobs has been increased.

--- configuration.nix
+++ configuration.nix
@@ -10,6 +10,8 @@
       /etc/nixos/hardware-configuration.nix
     ];
 
+  nix.maxJobs = 4;
+
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
